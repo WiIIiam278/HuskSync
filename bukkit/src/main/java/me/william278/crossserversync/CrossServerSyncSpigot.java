6742131d96936b3df690d67a4518a2e64a9b5045
William <will27528@gmail.com>
2021-10-17 13:11:02 +0100

    Initial commit

---
+++ bukkit/src/main/java/me/william278/crossserversync/CrossServerSyncSpigot.java
@@ -0,0 +1,17 @@
+package me.william278.crossserversync;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CrossServerSyncSpigot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ bungeecord/src/main/java/me/william278/crossserversync/CrossServerSyncBungeeCord.java
@@ -0,0 +1,16 @@
+package me.william278.crossserversync;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class CrossServerSyncBungeeCord extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
