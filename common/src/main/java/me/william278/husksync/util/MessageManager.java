c0f4012904e123a9c0b4ea5151d7e2c42e72bbb5
William <will27528@gmail.com>
2022-01-06 13:42:31 +0000

    Add @HarvelsX to contributors :)

--- common/src/main/java/me/william278/husksync/util/MessageManager.java
+++ common/src/main/java/me/william278/husksync/util/MessageManager.java
@@ -17,6 +17,7 @@ public class MessageManager {
     public static StringBuilder PLUGIN_INFORMATION = new StringBuilder().append("[HuskSync](#00fb9a bold) [| %proxy_brand% Version %proxy_version% (%bukkit_brand% v%bukkit_version%)](#00fb9a)\n")
             .append("[%plugin_description%](gray)\n")
             .append("[• Author:](white) [William278](gray show_text=&7Click to pay a visit open_url=https://youtube.com/William27528)\n")
+            .append("[• Contributors:](white) [HarvelsX](gray show_text=&7Code)\n")
             .append("[• Plugin Info:](white) [[Link]](#00fb9a show_text=&7Click to open link open_url=https://github.com/WiIIiam278/HuskSync/)\n")
             .append("[• Report Issues:](white) [[Link]](#00fb9a show_text=&7Click to open link open_url=https://github.com/WiIIiam278/HuskSync/issues)\n")
             .append("[• Support Discord:](white) [[Link]](#00fb9a show_text=&7Click to join open_url=https://discord.gg/tVYhJfyDWG)");
