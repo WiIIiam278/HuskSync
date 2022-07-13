8760fcea1fdccbc260b0707b3019994085ed3e5e
William <will27528@gmail.com>
2022-07-13 11:15:43 +0100

    Fix tests

---
+++ common/src/test/java/net/william278/husksync/logger/DummyLogger.java
@@ -0,0 +1,44 @@
+package net.william278.husksync.logger;
+
+import de.themoep.minedown.MineDown;
+import net.william278.husksync.util.Logger;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.logging.Level;
+
+public class DummyLogger extends Logger {
+
+    public DummyLogger() {
+    }
+
+    @Override
+    public void log(@NotNull Level level, @NotNull String message, @NotNull Exception e) {
+        System.out.println(level.getName() + ": " + message);
+        e.printStackTrace();
+    }
+
+    @Override
+    public void log(@NotNull Level level, @NotNull String message) {
+        System.out.println(level.getName() + ": " + message);
+    }
+
+    @Override
+    public void log(@NotNull Level level, @NotNull MineDown mineDown) {
+        System.out.println(level.getName() + ": " + mineDown.message());
+    }
+
+    @Override
+    public void info(@NotNull String message) {
+        System.out.println(Level.INFO.getName() + ": " + message);
+    }
+
+    @Override
+    public void severe(@NotNull String message) {
+        System.out.println(Level.SEVERE.getName() + ": " + message);
+    }
+
+    @Override
+    public void config(@NotNull String message) {
+        System.out.println(Level.CONFIG.getName() + ": " + message);
+    }
+}
