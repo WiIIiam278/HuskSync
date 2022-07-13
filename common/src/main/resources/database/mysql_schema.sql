112e5fe0bdedb6b1c4bf4a452d2d0a27fd287b09
William <will27528@gmail.com>
2022-07-13 16:39:12 +0100

    `longblob` rather than `mediumblob`

--- common/src/main/resources/database/mysql_schema.sql
+++ common/src/main/resources/database/mysql_schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS `%user_data_table%`
     `timestamp`    datetime    NOT NULL,
     `save_cause`   varchar(32) NOT NULL,
     `pinned`       boolean     NOT NULL DEFAULT FALSE,
-    `data`         mediumblob  NOT NULL,
+    `data`         longblob    NOT NULL,
     PRIMARY KEY (`version_uuid`, `player_uuid`),
     FOREIGN KEY (`player_uuid`) REFERENCES `%users_table%` (`uuid`) ON DELETE CASCADE
 );
