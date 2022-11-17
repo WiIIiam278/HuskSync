c4b194f8d62579b3bcdf14db813bd474d4abeb8b
William <will27528@gmail.com>
2022-11-17 02:46:40 +0000

    Fix unlocked maps getting wrongly locked

--- bukkit/src/main/java/net/william278/husksync/data/BukkitMapHandler.java
+++ bukkit/src/main/java/net/william278/husksync/data/BukkitMapHandler.java
@@ -77,7 +77,7 @@ public class BukkitMapHandler {
         }
 
         final MapMeta mapMeta = (MapMeta) itemStack.getItemMeta();
-        if (mapMeta == null) {
+        if (mapMeta == null || mapMeta.getMapView() == null || !mapMeta.getMapView().isLocked()) {
             return;
         }
 
