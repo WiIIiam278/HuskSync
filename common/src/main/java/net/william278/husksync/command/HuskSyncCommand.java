66bbde0b5de460ac40ed6fdc77fa6bb9faf92aba
William278 <will27528@gmail.com>
2024-01-19 16:33:56 +0000

    command: update translator credits in AboutMenu

--- common/src/main/java/net/william278/husksync/command/HuskSyncCommand.java
+++ common/src/main/java/net/william278/husksync/command/HuskSyncCommand.java
@@ -79,8 +79,9 @@ public class HuskSyncCommand extends Command implements TabProvider {
                         AboutMenu.Credit.of("DJelly4K").description("Simplified Chinese (zh-cn)"),
                         AboutMenu.Credit.of("Thourgard").description("Ukrainian (uk-ua)"),
                         AboutMenu.Credit.of("xF3d3").description("Italian (it-it)"),
-                        AboutMenu.Credit.of("cada3141").description("Korean (ko-kr)"))
-                        AboutMenu.Credit.of("Wirayuda5620").description("Bahasa Indonesia (id-id)"))
+                        AboutMenu.Credit.of("cada3141").description("Korean (ko-kr)"),
+                        AboutMenu.Credit.of("Wirayuda5620").description("Indonesian (id-id)"),
+                        AboutMenu.Credit.of("WinTone01").description("Turkish (tr-tr)"))
                 .buttons(
                         AboutMenu.Link.of("https://william278.net/docs/husksync").text("Documentation").icon("⛏"),
                         AboutMenu.Link.of("https://github.com/WiIIiam278/HuskSync/issues").text("Issues").icon("❌").color(TextColor.color(0xff9f0f)),
