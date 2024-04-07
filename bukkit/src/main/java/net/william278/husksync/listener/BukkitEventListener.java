7f75b9a9172ec17c301aa917158b057536a25b7f
William <will27528@gmail.com>
2024-04-07 12:35:22 +0100

    refactor: explicitly cancel ArmorStandManipulateEvent

--- bukkit/src/main/java/net/william278/husksync/listener/BukkitEventListener.java
+++ bukkit/src/main/java/net/william278/husksync/listener/BukkitEventListener.java
@@ -44,6 +44,7 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
 import org.bukkit.event.server.MapInitializeEvent;
 import org.bukkit.event.world.WorldSaveEvent;
 import org.bukkit.inventory.ItemStack;
@@ -160,6 +161,11 @@ public class BukkitEventListener extends EventListener implements BukkitJoinEven
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPlayerInteractEntity(@NotNull PlayerInteractEntityEvent event) {
+        cancelPlayerEvent(event.getPlayer().getUniqueId(), event);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPlayerInteractArmorStand(@NotNull PlayerArmorStandManipulateEvent event) {
         cancelPlayerEvent(event.getPlayer().getUniqueId(), event);
     }
 
