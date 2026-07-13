c2c1f992d7f891dc0b5ee8b9b2da2b9222ffa56f
Alexander Majka <alexander@primecodes.de>
2026-07-13 14:12:27 +0200

    Fix missing import

--- bukkit/src/main/java/net/william278/husksync/data/BukkitSerializer.java
+++ bukkit/src/main/java/net/william278/husksync/data/BukkitSerializer.java
@@ -39,6 +39,7 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static net.william278.husksync.data.BukkitData.Items.Inventory.INVENTORY_SLOT_COUNT;
